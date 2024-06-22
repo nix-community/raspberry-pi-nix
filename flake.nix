aa99f3a2ddc08a8c3c22f8a6ddcdd5ddd245a55e
adminy <marinbivol10@gmail.com>
2024-06-23 00:02:04 +0100

    chore: downgrade u-boot to stable release

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     u-boot-src = {
       flake = false;
-      url = "https://ftp.denx.de/pub/u-boot/u-boot-2024.07-rc4.tar.bz2";
+      url = "https://ftp.denx.de/pub/u-boot/u-boot-2024.04.tar.bz2";
     };
     rpi-linux-6_6-src = {
       flake = false;
