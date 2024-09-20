81707d7230d5151650220800515b1ff01a4c4959
Sebastian Sellmeier <mail@sebastian-sellmeier.de>
2024-09-20 13:52:24 +0200

    example: add pipewire

--- example/default.nix
+++ example/default.nix
@@ -34,4 +34,11 @@
       };
     };
   };
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 }
